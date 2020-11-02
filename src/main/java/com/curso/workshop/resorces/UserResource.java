21b3d0687a36875a9a9f1f1bdd97bb0b017fbd97
franciole <franciole.bezerra@gmail.com>
2020-11-02 20:27:04 -0300

    User entity and resource

---
+++ src/main/java/com/curso/workshop/resorces/UserResource.java
@@ -0,0 +1,19 @@
+package com.curso.workshop.resorces;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.curso.workshop.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmal.com", "(11) 99999-9999", "123456");
+				return ResponseEntity.ok().body(u);
+	}
+}
