73f5de65a7034be6b2d23b4b0d6b82e0947eb9a6
franciole <franciole.bezerra@gmail.com>
2020-11-08 00:12:18 -0300

    OrderItem, many-to-many association with extra attributes PART 1

--- src/main/java/com/curso/workshop/entities/Product.java
+++ src/main/java/com/curso/workshop/entities/Product.java
@@ -1,5 +1,6 @@
 package com.curso.workshop.entities;
 
+import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,11 +12,12 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
-public class Product {
+public class Product implements Serializable{
+
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
